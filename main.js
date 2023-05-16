88f8f45d09dbaccc21b7c52dbf2289be7c9ae005
portugalezee <54420731+ezequielportugal@users.noreply.github.com>
2023-05-16 06:46:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const typed = new Typed('.typed', {
     strings: [
         '<i class="soy">Ezequiel Portugal</i>',
         '<i class="soy">Programador</i>',
-        '<i class="soy">Electricista</i>',
-        '<i class="soy">Informático</i>'
+        '<i class="soy">Bachiller Informático</i>'
     ],
     loop: true,
     typeSpeed: 75,
